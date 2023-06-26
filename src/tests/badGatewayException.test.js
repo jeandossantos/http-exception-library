e9f640ba59d063bf8b64cc74250bdf4cc5d06169
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-26 14:52:46 -0300

    Test: add test for BadGatewayException  class

---
+++ src/tests/badGatewayException.test.js
@@ -0,0 +1,30 @@
+import assert from 'node:assert';
+import { test, describe } from 'node:test';
+import { STATUS_CODES } from 'node:http';
+
+import BadGatewayException from '../exceptions/badGatewayException.js';
+import { HttpStatus } from '../constants.js';
+
+describe('BadGatewayException class', () => {
+  const statusCode = HttpStatus.GATEWAY_TIMEOUT;
+
+  const defaultError = {
+    message: STATUS_CODES[statusCode],
+    statusCode,
+    error: 'USER_NOT_FOUND',
+  };
+
+  test('Should create a BadGatewayException with custom message and error', () => {
+    const exception = new BadGatewayException('Some Error', 'Invalid Username');
+
+    assert.deepStrictEqual(exception.message, 'Some Error');
+    assert.deepStrictEqual(exception.error, 'Invalid Username');
+  });
+  test('Should create a BadGatewayException object matching with {defaultError}', () => {
+    const exception = new BadGatewayException();
+
+    assert.deepStrictEqual(exception.message, defaultError.message);
+    assert.deepStrictEqual(exception.statusCode, defaultError.statusCode);
+    assert.deepStrictEqual(Reflect.has(exception, 'error'), false);
+  });
+});
