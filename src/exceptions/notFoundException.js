d42137b32a04cbc958d3d25c5671cc94ac117493
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-26 13:43:19 -0300

    Doc: doc NotFoundException class

--- src/exceptions/notFoundException.js
+++ src/exceptions/notFoundException.js
@@ -5,7 +5,16 @@ import { HttpStatus } from '../constants.js';
 
 const statusCode = HttpStatus.NOT_FOUND;
 
+/**
+ * Represents a Not Found exception.
+ * Extends the HttpException class.
+ */
 export default class NotFoundException extends HttpException {
+  /**
+   * Creates a new instance of NotFoundException.
+   * @param {string} [message] - The error message. Defaults to the corresponding HTTP status message.
+   * @param {*} [error] - The underlying error associated with the exception. Can be of any type.
+   */
   constructor(message = STATUS_CODES[statusCode], error) {
     super(message, statusCode, error);
   }
