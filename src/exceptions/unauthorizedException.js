e48acac74c869aa29417fdadea66b2d5bae86cba
Jean F. dos Santos <jeanddg@hotmail.com>
2023-06-26 13:38:47 -0300

    doc: doc UnauthorizedException class

--- src/exceptions/unauthorizedException.js
+++ src/exceptions/unauthorizedException.js
@@ -5,7 +5,16 @@ import { HttpStatus } from '../constants.js';
 
 const statusCode = HttpStatus.UNAUTHORIZED;
 
+/**
+ * Represents an Unauthorized exception.
+ * Extends the HttpException class.
+ */
 export default class UnauthorizedException extends HttpException {
+  /**
+   * Creates a new instance of UnauthorizedException.
+   * @param {string} [message] - The error message. Defaults to the corresponding HTTP status message.
+   * @param {*} [error] - The underlying error associated with the exception. Can be of any type.
+   */
   constructor(message = STATUS_CODES[statusCode], error) {
     super(message, statusCode, error);
   }
